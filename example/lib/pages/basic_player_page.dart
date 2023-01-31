231145e5357526ed78b43c9a89ff0202a48e209f
Fernando Ferreira <fernando.ferreira@grancursosonline.com.br>
2023-01-31 17:56:20 -0300

    ajustes

--- example/lib/pages/basic_player_page.dart
+++ example/lib/pages/basic_player_page.dart
@@ -41,7 +41,6 @@ class _BasicPlayerPageState extends State<BasicPlayerPage> {
           const SizedBox(height: 8),
           FutureBuilder<String>(
             future: Utils.getFileUrl(Constants.fileTestVideoUrl),
-            //future: Utils.getFileUrl("https://videoaulas.infra.grancursosonline.com.br/564afe295161b94c3dcc76ab8ec071b3/b859d03e9fec41dc9dd5ac0cbf6b1a51/b859d03e9fec41dc9dd5ac0cbf6b1a51-p480.mp4?Expires=1675276829&Signature=ZZzh2NgoRv~7OvchUxUH3mGKdHOSYDEw7jTJlEoVn6Xvk-1PfclGH2vQ-6XqK42qULqTD39a2ah9fa20K7mr5ckjggyAMqkOboWa41CFXdRvrIw3I6X6Oow1luVOHrO4YqhT4XNsJg6aStcJwhTj-EkH4iIoOY0vXHhV8v9MuA6R4T2UCuPb-ot~qsze8kHBtjXfNHMZmxKI0EUVKGCAetB-W3gZX2J3FtyI9axKLUuJ6lMud1e7LFfeF2m4Onx2MkJujiHdHN1Le72HlF7TO2e1lUmtr0En9NTvmrwgZYT-dg6zPLey~DfVM9MyWqD6PbFkyQ8k4tvo5usxSYuL8g__&Key-Pair-Id=APKAJWDRH5QWMLF2KNSA"),
             builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(child: CircularProgressIndicator());
